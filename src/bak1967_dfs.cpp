5e2528ed64c191974a0d8dc2f8e4d84e3242beb0
yoon jaehee <ytjh05@naver.com>
2022-01-20 13:04:49 +0900

    dfs twice

--- src/bak1967_dfs.cpp
+++ src/bak1967_dfs.cpp
@@ -1,31 +1,37 @@
 //트리의 지름 구하기... dfs 이용.
 #include <iostream>
 #include <vector>
+#include <cstring>
 using namespace std;
-int n,temp,end,res;
+int n,temp,ed,res;
 vector<pair<int,int>> graph[10001];
 bool visited[10001]={false,};
 void dfs(int start,int len){
+    if(visited[start]){
+        return;
+    }
+    if(res < len){
+        res=len;
+        ed=start;
+    }
     visited[start]=true;
     for(int t=0;t<graph[start].size();t++){
         pair<int,int> cur=graph[start][t];
-        if(!visited[cur.first]){
-            dfs(cur.first,cur.second);
-            end=cur.first;
-            res=cur.second;
-        }
+        dfs(cur.first,len+cur.second);
     }
 }
 int main(){
     int s,e,t;
+    ed=0;
+    res=0;
     scanf("%d",&n);
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n-1;i++){
         scanf("%d %d %d",&s,&e,&t);
         graph[s].push_back({e,t});
         graph[e].push_back({s,t});
     }
     dfs(1,0);
     memset(visited,false,sizeof(visited));
-    dfs(end,0);
-    printf("%d",len);
+    dfs(ed,0);
+    printf("%d",res);
 }
