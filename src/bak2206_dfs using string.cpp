fd244eca2dff63b61f7d448731896aba3c6d62f0
yoon jaehee <ytjh05@naver.com>
2022-01-21 17:05:55 +0900

    웨안돼...

--- src/bak2206_dfs using string.cpp
+++ src/bak2206_dfs using string.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <cstring>
 using namespace std;
 int n,m;
+int px[4]={0,0,-1,1};
+int py[4]={1,-1,0,0};
 char graph[1001][1001]={0,};
+queue<pair<pair<int,int>,int>> q;
+int visited[1001][1001][2]={0,};
+int bfs(){
+    q.push({{0,0},1});
+    visited[0][0][1]=1;
+    while(!q.empty()){
+        int cur_x = q.front().first.second;
+        int cur_y = q.front().first.first;
+        int block = q.front().second;
+        q.pop();
+        if((cur_x == m) && (cur_y == n)){
+            return visited[cur_y][cur_x][block];
+        }
+        for(int i=0;i<4;i++){
+            int nx = cur_x + px[i];
+            int ny = cur_y + py[i];
+            if(nx>=0 && nx<m && ny>=0 && ny<n){
+                if(graph[ny][nx]=='0' && visited[ny][nx][block] == 0){
+                    visited[ny][nx][block] = visited[cur_y][cur_x][block]+1;
+                    q.push({{ny,nx},block});
+                }
+                if(graph[ny][nx]=='1' && block){
+                    visited[ny][nx][block-1] = visited[cur_y][cur_x][block]+1;
+                    q.push({{ny,nx},block-1});
+                }
+            }
+        }
+    }
+    return -1;
+}
 int main(){
-    string s;
+    int res;
     scanf("%d %d",&n,&m);
     for(int i=0;i<n;i++){
         scanf("%s",&graph[i][0]);
     }
-    printf("%c",graph[1][1]);
+    res = bfs();
+    printf("%d",res);
 }
