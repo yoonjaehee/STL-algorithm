5ee0282caf8c974b29202c68d2a1ffdc5f0d9d0f
yoon jaehee <ytjh05@naver.com>
2022-02-03 11:39:04 +0900

    dp+ string problem solved

--- src/bak2263_tree.cpp
+++ src/bak2263_tree.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -34,26 +33,3 @@ int main(){
     }
     getpreorder(1,n,1,n);
 }
-||||||| merged common ancestors
-=======
-#include <iostream>
-#include <vector>
-using namespace std;
-int n;
-vector<int> post,in;
-void pre(){
-    
-}
-int main(){
-    scanf("%d",&n);
-    post = vector<int>(n,0);
-    in = vector<int>(n,0);
-    for(int i=0;i<n;i++){
-        scanf("%d",&in[i]);
-    }
-    for(int i=0;i<n;i++){
-        scanf("%d",&post[i]);
-    }
-    pre();
-}
->>>>>>> b87cacedea254b3181d994c451ae2924d214b18d
---
+++ src/bak2409_dp+string.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int n,m;
+string arr[101][101];
+string adding(string num1,string num2){
+    int sum=0;
+    string res;
+    while(!num1.empty() || !num2.empty() || sum){
+        if(!num1.empty()){
+            sum+=num1.back() -'0';
+            num1.pop_back();
+        }
+        if(!num2.empty()){
+            sum+=num2.back() - '0';
+            num2.pop_back();
+        }
+        res.push_back((sum%10)+'0');
+        sum/=10;
+    }
+    reverse(res.begin(),res.end());
+    return res;
+}
+string comb(int a,int b){
+    if(a==b || b==0){
+        return "1";
+    }
+    string &res = arr[a][b];
+    if((a/2 == 0) && (b > a/2)){
+        b = a-b; 
+    }
+    if(res != ""){
+        return res;
+    }
+    res = adding(comb(a-1,b-1),comb(a-1,b));
+    return res;
+}
+int main(){
+    scanf("%d %d",&n,&m);
+    printf("%s",comb(n,m));
+}
