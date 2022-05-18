c0ca0a6257db465d32a76072d10033465fd39747
yoon jaehee <ytjh05@naver.com>
2022-05-18 17:01:02 +0900

    5hours gone....because of misunderstanding problem

---
+++ src/bak2149(암호문으로 바꿈...)_string.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+vector<pair<char,int>> vec;
+vector<char>v[101];
+int main(){
+    string temp,s;
+    getline(cin,temp);
+    vector<char> v[101];
+    for(int i=0;i<temp.size();i++){
+        vec.push_back({temp[i],i});
+    }
+    sort(vec.begin(),vec.end());
+    for(int i=0;i<temp.size();i++){
+        cout<<vec[i].first<<" "<<vec[i].second<<endl;
+    }
+    getline(cin,s);
+    int count = s.size()/temp.size();
+    char res[temp.size()][count]={0,};
+    for(int i=0;i<count;i++){
+        for(int t=0;t<temp.size();t++){
+            v[i].push_back(s[i*temp.size() + t]);
+            //res[i][vec[t].second] = v[i][t];
+        }
+    }
+    for(int i=0;i<count;i++){
+        for(int t=0;t<temp.size();t++){
+            res[i][vec[t].second] = v[i][t];
+            //res[i][vec[t].second] = v[i][t];
+        }
+    }
+    for(int i=0;i<temp.size();i++){
+        for(int t=0;t<count;t++){
+            cout<<res[t][i];
+        }
+        cout<<endl;
+    }
+}
