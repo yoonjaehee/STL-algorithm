b18c5adb2ce17189d502d9661a13c408de7495d7
yoon jaehee <ytjh05@naver.com>
2022-05-19 15:56:07 +0900

    string solved

--- src/bak2149_string.cpp
+++ src/bak2149_string.cpp
@@ -4,35 +4,26 @@
 #include <algorithm>
 using namespace std;
 vector<pair<char,int>> vec;
-vector<char>v[101];
+char v[101][101];
 int main(){
     string temp,s;
     getline(cin,temp);
-    vector<char> v[101];
     for(int i=0;i<temp.size();i++){
         vec.push_back({temp[i],i});
     }
     sort(vec.begin(),vec.end());
-    for(int i=0;i<temp.size();i++){
-        cout<<vec[i].first<<" "<<vec[i].second<<endl;
-    }
     getline(cin,s);
     int count = s.size()/temp.size();
     char res[temp.size()][count]={0,};
     for(int i=0;i<count;i++){
         for(int t=0;t<temp.size();t++){
-            v[i].push_back(s[i*temp.size() + t]);
+            v[i][vec[t].second] = s[t*count + i];
         }
     }
     for(int i=0;i<count;i++){
         for(int t=0;t<temp.size();t++){
-            res[i][vec[t].second] = v[i][t];
+            printf("%c",v[i][t]);
         }
     }
-    for(int i=0;i<temp.size();i++){
-        for(int t=0;t<count;t++){
-            cout<<res[t][i];
-        }
-        cout<<endl;
-    }
+
 }
