d02001e1121e5a0c95dc9f9fb85ef669f191e437
yoon jaehee <ytjh05@naver.com>
2022-12-07 10:33:16 +0900

    samsung completed

--- src/implementation/bak12100.cpp
+++ src/implementation/bak12100.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <string.h>
 #include <vector>
+#include <queue>
 using namespace std;
 int n,mx,temp;
-int dfs(vector<vector<int>> tem){ 
+vector<int> res;
+priority_queue<int> pq;
+void dfs(vector<vector<int>> tem,int count){ 
     vector<vector<int>> temp;
     temp = tem;
     //위로 이동
+    if(count>4){
+        pq.push(mx);
+        return;
+    }
     for(int i=1;i<n;i++){
         for(int t=0;t<n;t++){
             if(temp[i][t] == temp[i-1][t]){ // 위에 숫자가 똑같을때 : 합치고 밀기
@@ -17,17 +24,21 @@ int dfs(vector<vector<int>> tem){
     }
     for(int i=1;i<n;i++){
         for(int t=0;t<n;t++){
+            if(mx < temp[i][t]){
+                mx = temp[i][t];
+            }
             for(int p=i;p>0;p--){
-                if(temp[p-1][t] == 0){
+                if(temp[p-1][t] == 0 && temp[p][t] != 0){
                     temp[p-1][t] = temp[p][t];
+                    temp[p][t]=0;
                 }
             }
         }
     }
-    dfs(temp);
+    dfs(temp,count+1);
     temp = tem;
     //아래로 이동
-    for(int i=n-1;i>0;i++){
+    for(int i=n-1;i>0;i--){
         for(int t=0;t<n;t++){
             if(temp[i-1][t] == temp[i][t]){ // 위에 숫자가 똑같을때 : 합치고 밀기
                 temp[i-1][t] = 0;
@@ -35,39 +46,68 @@ int dfs(vector<vector<int>> tem){
             }
         }
     }
-    for(int i=n-1;i>0;i++){
+    for(int i=n-1;i>0;i--){
         for(int t=0;t<n;t++){
+            if(mx < temp[i][t]){
+                mx = temp[i][t];
+            }
             for(int p=i;p<n;p++){
-                if(temp[p+1][t] == 0){
+                if(temp[p+1][t] == 0 && temp[p][t] != 0){
                     temp[p+1][t] = temp[p][t];
                     temp[p][t]=0;
                 }
             }
         }
     }
-    dfs(temp);
+    dfs(temp,count+1);
     temp = tem;
     //오른쪽으로 밀음
-    for(int i=0;i<n;i++){
-        for(int t=0;t<n-1;t++){
-            if(temp[i][t] == temp[i][t+1]){ // 위에 숫자가 똑같을때 : 합치고 밀기
+    for(int t=0;t<n-1;t++){
+        for(int i=0;i<n;i++){
+            if(temp[i][t] == temp[i][t+1]){
                 temp[i][t] = 0;
-                temp[i][t+1]*=2;
+                temp[i+1][t]*=2;
             }
         }
-    }
-    for(int i=0;i<n;i++){
-        for(int t=0;t<n-1;t++){
+    }    
+    for(int t=n-1;t>0;t++){
+        for(int i=0;i<n;i++){
+            if(mx < temp[i][t]){
+                mx = temp[i][t];
+            }
             for(int p=t;p<n;p++){
-                if(temp[i][p] == 0){
-                    temp[i][p] = temp[i][t];
+                if(temp[i][p+1] == 0 && temp[i][p] != 0){
+                    temp[i][p+1] = temp[i][p];
+                    temp[i][p] = 0;
                 }
             }
         }
     }
-    dfs(temp);
+    dfs(temp,count+1);
     temp = tem;
     //왼쪽으로 밀음
+    for(int t=n;t>1;t--){
+        for(int i=0;i<n;i++){
+            if(temp[i][t] == temp[i][t-1]){
+                temp[i][t] = 0;
+                temp[i-1][t]*=2;
+            }
+        }
+    }    
+    for(int t=1;t<n;t++){
+        for(int i=0;i<n;i++){
+            if(mx < temp[i][t]){
+                mx = temp[i][t];
+            }
+            for(int p=t;p>n;p--){
+                if(temp[i][p+1] == 0 && temp[i][p] != 0){
+                    temp[i][p+1] = temp[i][p];
+                    temp[i][p] = 0;
+                }
+            }
+        }
+    }
+    dfs(temp,count+1);
 
 }
 int main(){
@@ -80,5 +120,5 @@ int main(){
             vec[i].push_back(temp);
         }
     }
-    dfs(vec);
+    dfs(vec,0);
 }
