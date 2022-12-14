df88443c9167d81017399330fa9bf028eb8f2fd4
yoon jaehee <ytjh05@naver.com>
2022-12-14 16:06:53 +0900

    dp fixed

--- src/dp/bak12852_2.cpp
+++ src/dp/bak12852_2.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <cstring>
 using namespace std;
+int dp[100001];
+int before[100001];
 int main(){
     int x;
-    vector<int> temp;
     scanf("%d",&x);
-    int dp[100001]={0,};
-    dp[]
+    memset(dp,0,sizeof(int)*(x+1));
+    memset(before,0,sizeof(int)*(x+1));
+    before[1] = -1;
+    for(int i=2;i<=x;i++){
+        dp[i] = dp[i-1] + 1;
+        before[i] = i-1;
+        if((i%2 == 0) && (dp[i] > dp[i/2] + 1)){
+            dp[i] = dp[i/2] + 1;
+            before[i] = i/2;
+        }
+        if((i%3 == 0) && dp[i] > dp[i/3] + 1){
+            dp[i] = dp[i/3] + 1;
+            before[i] = i/3;
+        }
+    }
+    printf("%d\n",dp[x]);
+    int temp = x;
+    while(temp != 1){
+        printf("%d ",temp);
+        temp = before[temp];
+    }
+    printf("1");
+
 }
