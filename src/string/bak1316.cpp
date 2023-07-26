8e0101d42c72a8c416a2ed568609ae78aee4db5c
yoonjaehee <46480817+ytjh05@users.noreply.github.com>
2023-07-26 20:10:25 +0900

    solveD

--- src/string/bak1316.cpp
+++ src/string/bak1316.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
+#include <vector>
 #include <string.h>
 using namespace std;
+bool arr[27] = {false,};
 int main(){
     int n;
     int count = 0;
-    int arr[100] = {0,};
     scanf("%d",&n);    
     string str;
+    char cur = 0;
+    cin.ignore();
     for(int i=0;i<n;i++){
+        memset(arr,false,sizeof(bool)*27);
         getline(cin,str);
-        for(int t=0;t<str.size()-1;t++){
-            if(arr[str[t+1]] != 0){
+        for(int t=0;t<str.size();t++){
+            if(arr[str[t]-'a'] && cur != str[t]){
                 count++;
+                break;
             }
-            if(str[t] != str[t+1]){
-                arr[str[t]]++;
+            else{
+                arr[str[t]-'a'] = true;
             }
+            cur = str[t];
         }
     }
     printf("%d",n-count);
