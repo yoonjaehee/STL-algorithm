fcab79d9e15f85567d46f25fd277d667687dbb7b
yoonjaehee <46480817+ytjh05@users.noreply.github.com>
2024-02-02 21:46:46 +0900

    samsung solved

--- src/samsung/불안한 무빙워크.cpp
+++ src/samsung/불안한 무빙워크.cpp
@@ -1,69 +1,68 @@
 #include <iostream>
+#include <vector>
+#include <string.h>
 using namespace std;
 int n,k;
-int arr[2][50];
-bool visited[50];
-int res= 0;
-void checking(){
-    for(int i=n-1;i>=0;i--){
-        if(i== n-1){
-            visited[i] = false;
-            continue;
+vector<int> vec;
+bool visited[101];
+long long res= 0;
+void dfs(int start,long long cnt){
+    if(start >= k){
+        res = cnt;
+        return;
+    }
+    // 1. 무빙워크가 한 칸 회전
+    int st = vec[2*n-1];
+    for(int i=2*n-1;i>0;i--){
+        vec[i] = vec[i-1];
+        if(i <= n-1){
+            visited[i] = visited[i-1];
         }
-        if(visited[i] && arr[0][i+1] > 0){
-            visited[i] = false;
-            visited[i+1] = true;
-            arr[0][i+1]--;
-        }
     }
-
-}
-void move(){
-    int tmp = arr[0][n-1];
-    for(int i=n-1;i>=0;i--){
-        arr[0][i] = arr[0][i-1];
-        visited[i] = false;
-        visited[i+1] = true;
+    vec[0] = st;
+    visited[0] = false;
+    if(visited[n-1]){
+        visited[n-1] = false;
     }
-    arr[0][0] = arr[1][0];
-    for(int i=0;i<n-1;i++){
-        arr[1][i] = arr[1][i+1];
-    }
-    arr[1][n-1] = tmp;
-}
-int cnt;
-int walk(){
-    if(cnt >= k){
-        return 0;
-    }
-    move(); // 무빙워크 한칸씩
-    checking(); // 사람 옮기기
-    if(arr[0][0]>0 && !visited[0]){
-        arr[0][0]--;
-        visited[0]= true;
-    }
-    cnt = 0; // 0인갯수확인
-    for(int i=0;i<2;i++){
-        for(int t=0;t<n;t++){
-            if(arr[i][t] <= 0){
-                cnt++;
+    // 2. 비어있으면 한칸 이동
+    for(int i=n-2;i>=1;i--){
+        if(visited[i]){
+            if(!visited[i+1] && (vec[i+1] >= 1)){
+                visited[i+1] = true;
+                vec[i+1]-=1;
+                visited[i] = false;
+                if(vec[i+1] == 0){
+                    start+=1;
+                }
             }
         }
     }
-    res++;
-    walk();
+    if(visited[n-1]){
+        visited[n-1] = false;
+    }
+    // 3. 1번칸에 사람이 없고 안정성이 0이 아니라면 사람을 한명 더 올림
+    if(!visited[0] && vec[0] != 0){
+        vec[0]-=1;
+        visited[0] = true;
+        if(vec[0] == 0){
+            start+=1;
+        }
+    }
+    // 4. 안정성이 0인 칸이 k개이상이면 과정 종료
+    if(start >= k){
+        res = cnt;
+        return;
+    }
+    dfs(start,cnt+1);
 }
 int main(){
     scanf("%d %d",&n,&k);
+    memset(visited,false,sizeof(bool)*(n+1));
     int temp;
-    for(int i=0;i<n;i++){
+    for(int i=0;i<2*n;i++){
         scanf("%d",&temp);
-        arr[0][i] = temp;
+        vec.push_back(temp);
     }
-    for(int i=n-1;i>=0;i--){
-        scanf("%d",&temp);
-        arr[1][i] = temp;
-    }
-    walk();
-    printf("%d",res);
+    dfs(0,1);
+    printf("%lld",res);
 }
